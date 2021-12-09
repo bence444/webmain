921ccd3ef862de65cb734e675c844ca70f04b324
bence444 <lovasz.b99@gmail.com>
2021-12-09 23:20:26 +0100

    added navbar

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
+		<script src="https://kit.fontawesome.com/5419a78a39.js" crossorigin="anonymous"></script>
+
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet">
@@ -16,16 +18,20 @@
 		<div class="bg">
 			<div class="shadow">
 				<div class="d-flex justify-content-center align-items-end h-50 text-light pb-0 my"><h1 class="display-2 fade-in">Bence Lovász</h1></div>
-				<div class="d-flex justify-content-center align-items-start h-50 text-light pt-0"><h4 class="h4 text-warning slide-fade-in" style="font-weight: bolder; letter-spacing: 29px; margin-right: -29px;">lorem ipsum</h4></div>
+				<div class="d-flex justify-content-center align-items-start h-50 text-light pt-0"><h4 class="h4 text-info-light slide-fade-in" style="font-weight: bolder; letter-spacing: 29px; margin-right: -29px;">lorem ipsum</h4></div>
 			</div>
 		</div>
 
-		<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+		<div class="w-75 mt-5 bg-info mx-auto border border-info rounded-5 ps-2 pe-3">
+			<div class="d-flex flex-row justify-content-start align-items-center" style="height: 50px;">
+				<a class="active btn btn-info text-light me-2">home</a>
+				<a class="btn btn-info text-light me-2">pages</a>
+				<a class="btn btn-info text-light me-auto">contact</a>
+
+				<a class="text-light cursor"><i class="fab fa-lg fa-twitter-square me-2"></i></a>
+				<a class="text-light cursor"><i class="fab fa-lg fa-github-square"></i></a>
+			</div>
+		</div>
 	</body>
 
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js"></script>
