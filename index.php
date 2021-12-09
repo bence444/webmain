34ab207e2d8b99dfd09ff10d32b2b127cf8c3633
bence444 <lovasz.b99@gmail.com>
2021-12-09 21:40:15 +0100

    Update index.php
    added mdb

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title></title>
+
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet">
+
+		<title>H o m e p a g e</title>
 	</head>
 
 	<body>
 		<p style="text-align: center">Soon</p>
 	</body>
+
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js"></script>
 </html>
