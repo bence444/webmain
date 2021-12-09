9ac9081a542701e19fdcec7167e0a5b790ec716f
bence444 <lovasz.b99@gmail.com>
2021-12-09 22:34:07 +0100

    added header

--- index.php
+++ index.php
@@ -7,12 +7,25 @@
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
 
 		<title>H o m e p a g e</title>
 	</head>
 
-	<body>
-		<p style="text-align: center">Soon</p>
+	<body class="bg-dark text-light">
+		<div class="bg">
+			<div class="shadow">
+				<div class="d-flex justify-content-center align-items-end h-50 text-light pb-0 my"><h1 class="display-2 fade-in">Bence Lovász</h1></div>
+				<div class="d-flex justify-content-center align-items-start h-50 text-light pt-0"><h4 class="h4 text-warning slide-fade-in" style="font-weight: bolder; letter-spacing: 29px; margin-right: -29px;">lorem ipsum</h4></div>
+			</div>
+		</div>
+
+		<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 	</body>
 
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.1/mdb.min.js"></script>
