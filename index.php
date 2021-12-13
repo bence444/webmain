68da44b232fbd0577b50df807f1a47bc43f87423
bence444 <lovasz.b99@gmail.com>
2021-12-12 17:48:14 -0800

    removed navbar, added pages list under header

--- index.php
+++ index.php
@@ -13,26 +13,57 @@
 		
 		<link href="css/style.css" rel="stylesheet">
 
-		<title>H o m e p a g e</title>
+		<title>h o m e p a g e</title>
 	</head>
 
 	<body class="bg-dark text-light">
 		<div class="bg">
 			<div class="shadow">
 				<div class="d-flex justify-content-center align-items-end h-50 text-light pb-0 my"><h1 class="display-2 fade-in">Bence Lovász</h1></div>
-				<div class="d-flex justify-content-center align-items-start h-50 text-light pt-0"><h4 class="h4 text-info-light slide-fade-in" style="font-weight: bolder; letter-spacing: 29px; margin-right: -29px;">lorem ipsum</h4></div>
+				<div class="d-flex justify-content-center align-items-start h-50 text-light pt-0"><h4 class="h4 text-info slide-fade-in" style="font-weight: bolder; letter-spacing: 29px; margin-right: -29px;">lorem ipsum</h4></div>
 			</div>
 		</div>
 
-		<div class="w-75 mt-5 bg-info mx-auto border border-info rounded-5 ps-2 pe-3">
+		<!-- <div class="w-75 mt-5 bg-info mx-auto border border-info rounded-5 ps-2 pe-3">
 			<div class="d-flex flex-row justify-content-start align-items-center" style="height: 50px;">
 				<a class="active btn btn-info text-light me-2">home</a>
 				<a class="btn btn-info text-light me-2">school</a>
 				<a class="btn btn-info text-light me-2">pages</a>
 				<a class="btn btn-info text-light me-auto">contact</a>
 
-				<a class="text-light cursor"><i class="fab fa-lg fa-twitter-square me-2"></i></a>
-				<a class="text-light cursor"><i class="fab fa-lg fa-github-square"></i></a>
+				<a class="text-light cursor" href="https://twitter.com/yngnbs"><i class="fab fa-lg fa-twitter-square me-2"></i></a>
+				<a class="text-light cursor" href="https://github.com/bence444"><i class="fab fa-lg fa-github-square"></i></a>
+			</div>
+		</div> -->
+
+		<div class="w-75 row p-0 mx-auto mt-5 pt-5">
+			<h3 class="text-center my-3">p a g e s</h3>
+
+			<div class="col-md-4">
+				<div class="m-1 rounded bg-info border border-info">
+					<div class="w-100 bg-light" style="border-top-left-radius: 2px; border-top-right-radius: 2px; height: 200px; background: url('https://codeweek.eu/img/2021/challenges/thumbnails/paper-circuit.png'); background-position: center;">
+					</div>
+
+					<button class="btn btn-info m-2">preview</button>
+				</div>	
+			</div>
+
+			<div class="col-md-4">
+				<div class="m-1 rounded bg-info border border-info">
+					<div class="w-100 bg-light" style="border-top-left-radius: 2px; border-top-right-radius: 2px; height: 200px; background: url('https://codeweek.eu/img/2021/challenges/thumbnails/paper-circuit.png'); background-position: center;">
+					</div>
+
+					<button class="btn btn-info m-2">preview</button>
+				</div>	
+			</div>
+
+			<div class="col-md-4">
+				<div class="m-1 rounded bg-info border border-info">
+					<div class="w-100 bg-light" style="border-top-left-radius: 2px; border-top-right-radius: 2px; height: 200px; background: url('https://codeweek.eu/img/2021/challenges/thumbnails/paper-circuit.png'); background-position: center;">
+					</div>
+
+					<button class="btn btn-info m-2">preview</button>
+				</div>	
 			</div>
 		</div>
 	</body>
